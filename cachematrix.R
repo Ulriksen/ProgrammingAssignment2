da4499046f572c824292fc960efa5c9778099ce8
Harald Schult Ulriksen <harald@ulriksen.net>
2015-02-16 20:05:23 +0100

    Added implementation of method bodies

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module contains two methods which enables caching of the inverse of a matrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix takes a matrix and returns a function which 
+## use closures and module pattern to support caching of solve operation
+## The solve is stored in an internal variable m. 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(
+    set = set, 
+    get = get,
+    setSolve = setSolve,
+    getSolve = getSolve)
 
 }
 
-
-## Write a short comment describing this function
-
+##Takes a cacheMatrix object solves the matrix. If the matrix is already solved
+##the result will be returned from the cached value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getSolve()
+    if (!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setSolve(m)
+    m
 }
