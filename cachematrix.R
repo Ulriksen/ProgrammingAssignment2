1add0b4ea80dca529d8e922f3afc1cc54b60d05b
Harald Schult Ulriksen <harald@ulriksen.net>
2015-02-16 20:18:18 +0100

    Expanded the comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,12 @@
 ## This module contains two methods which enables caching of the inverse of a matrix
 
-## makeCacheMatrix takes a matrix and returns a function which 
-## use closures and module pattern to support caching of solve operation
+## makeCacheMatrix takes a matrix and returns a function which use 
+## closures and a list of functions to support caching of solve operation.
+## This follows the module pattern. 
 ## The solve is stored in an internal variable m. 
+## Calling set will clear the cached value. The 
+## cahced value can be retrived using getSolve and set using setSolve.
+## to retrieve the matrix, call the get function in list. 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -20,12 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-##Takes a cacheMatrix object solves the matrix. If the matrix is already solved
-##the result will be returned from the cached value.
+## Takes a list created by the makeCacheMatrix and returns an inverse matrix.
+## If the matrix is already solved it will be returned from cache. If not 
+## the result will be calculated using solve on the matrix in the list from
+## the makeCacheMatrix instance.
 cacheSolve <- function(x, ...) {
     m <- x$getSolve()
     if (!is.null(m)) {
-      message("getting cached data")
+      message("getting cached solve")
       return(m)
     }
     data <- x$get()
